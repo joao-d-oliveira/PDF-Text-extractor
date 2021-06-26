39db21b90a164d2d600753961c99309280db0475
Joao Oliveira <joao.d.oliveira@gmail.com>
2021-06-26 19:21:31 -0300

    added functions: split_files, get_txt_from_page, get_txt_allpages

--- extractPDFcolumns.py
+++ extractPDFcolumns.py
@@ -1,9 +1,10 @@
-import PyPDF2
-import numpy as np
-import pandas as pd
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+COUNT_SKIPS = 1
+COUNT_COLS = 4
 
 
-def treatText(txt):
+def treat_text(txt):
     import re
     out = []
     startedStr = False
@@ -17,24 +18,50 @@ def treatText(txt):
     return "".join(out)
 
 
-if __name__ == '__main__':
-    COUNT_SKIPS = 1
-    COUNT_COLS = 4
-    # creating a pdf reader object
-    fileReader = PyPDF2.PdfFileReader(open('pdfs/vacinacao_covid_COMPLETO_CE_29_03_2021.pdf', 'rb'))
-    # print the number of pages in pdf file
-    page = fileReader.getPage(0)
+def split_files(reader, nsplit):
+    n = fileReader.numPages
+    n = 11539 #overwrite page number given pdf is broken and empty after page 11539
+    k = n // nsplit
+    for i in range(0, n, k):
+        writer = PdfFileWriter()
+        for d in range(i, min(i + k, n), 1): writer.addPage(reader.getPage(d))
 
+        writer.write(open(f'output/output-{i}_{d}.pdf', 'wb'))
+
+
+def get_txt_from_page(page):
     cont = page.get('/Contents')
     pageTxt = ""
     for c in cont:
         obj_data = c.getObject().flateEncode().getData()
         obj_data = obj_data.decode('latin', 'replace')
+        # print([o for o in obj_data.split('\n') if 'TJ' in o.upper()])
 
-        pageTxt += ";".join([treatText(o) for o in obj_data.split('\n') if 'TJ' in o.upper()])
+        pageTxt += ";".join([treat_text(o) for o in obj_data.split('\n') if 'TJ' in o.upper()])
 
     pageTxt = [txt for txt in pageTxt.split(';') if 'Página' not in txt]
 
-    print(fileReader.numPages)
+    return pageTxt
 
+
+def get_txt_allpages(pdf):
+    n = pdf.numPages
+    n = 11539 #overwrite page number given pdf is broken and empty after page 11539
+    fullPDFtxt = ""
+    for i in range(n):
+        fullPDFtxt += get_txt_from_page(pdf.getPage(i))
+
+    return fullPDFtxt
+
+
+if __name__ == '__main__':
+
+    # creating a pdf reader object
+    fileReader = PdfFileReader(open('pdfs/vacinacao_covid_COMPLETO_CE_29_03_2021.pdf', 'rb'))
+
+    fulltxt = get_txt_allpages(fileReader)
+
+    #splitFiles(fileReader, 100)
+
+    print('done analysing')
 
